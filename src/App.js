02c295be263098dded36e754d44027a8ee3b5f98
mfcanesesr <mfcanesesr@gmail.com>
2024-02-19 10:23:52 -0600

    Ready player me

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  AssetUnlockedEvent,
+  AvatarCreator,
+  AvatarCreatorConfig,
+  AvatarExportedEvent,
+  UserAuthorizedEvent,
+  UserSetEvent,
+} from "@readyplayerme/react-avatar-creator";
+
+const config = {
+  clearCache: true,
+  bodyType: "fullbody",
+  quickStart: false,
+  language: "en",
+};
+
+const style = { width: "100%", height: "100vh", border: "none", margin: 0 };
 
 function App() {
+  const handleOnUserSet = (event) => {
+    console.log(`User ID is: ${event.data.id}`);
+  };
+
+  const handleOnAvatarExported = (event) => {
+    console.log(`Avatar URL is: ${event.data.url}`);
+  };
+
+  const handleUserAuthorized = (event) => {
+    console.log(`User is:`, event.data);
+  };
+
+  const handleAssetUnlocked = (event) => {
+    console.log(`Asset unlocked is: ${event.data.assetId}`);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <AvatarCreator
+        subdomain="fer.readyplayer.me?frameApi"
+        config={config}
+        style={style}
+        onAvatarExported={handleOnAvatarExported}
+        onUserAuthorized={handleUserAuthorized}
+        onAssetUnlock={handleAssetUnlocked}
+        onUserSet={handleOnUserSet}
+      />
     </div>
   );
 }
